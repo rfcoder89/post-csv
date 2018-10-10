9b8bb0150fda4bf18581d20ad1fc60fa6bb2270f
Richard.Flood <Richard.Flood@aveva.com>
2018-10-11 00:17:35 +0100

    basic implementation, dry-run feature and sample data (no error handling)

---
+++ index.js
@@ -0,0 +1,46 @@
+const process = require('process');
+const fs = require('fs');
+const program = require('commander');
+const axios = require('axios');
+
+program
+  .version('1.0')
+  .option('-f, --file <value>', 'input csv file')
+  .option('-u, --url <value>', 'server endpoint')
+  .option('-n, --dry-run', 'perform a dry run')
+  .parse(process.argv);
+
+const postParams = fs
+      .readFileSync(program.file || './input.csv', 'utf8')
+      .replace(/\r\n/g, '\n')
+      .split('\n')
+      .map(line => line
+           .split(',')
+           .reduce((acc, cur, index) => {
+             switch(index) {
+             case 0: acc.name = cur; break;
+             case 1: acc.param1 = cur; break;
+             case 2: acc.param2 = cur; break;
+             }
+             return acc;
+           }, {})
+          );
+
+if (!program.dryRun) {
+  Promise.all(
+    postParams
+      .map(param => axios.post(program.url, param))
+  ).then(responses => {
+    responses.forEach(res => {
+      console.log(res);
+    });
+  });
+}
+else {
+  console.log('performing a dry run');
+  postParams.forEach(param => {
+    console.log(param);
+  });
+}
+
+
